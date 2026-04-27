13583388445d9f753465babfc408efed99592a84
Maxens Verron <maxens.verron+github@outlook.com>
2026-04-27 21:02:29 +0200

    home and search components

--- webclient/src/app/app.routes.ts
+++ webclient/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Search } from './pages/search/search';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: Home },
+    { path: 'search', component: Search }
+];
