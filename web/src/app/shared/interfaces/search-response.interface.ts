b09169c457e342acc87ce55583a90174ffe50bea
Maxens Verron <maxens.verron+github@outlook.com>
2026-04-27 20:52:23 +0200

    remove web client

--- web/src/app/shared/interfaces/search-response.interface.ts
+++
@@ -1,8 +0,0 @@
-import { TMDbSearchResult } from './tmdb-trending.interface';
-
-export interface SearchResponse {
-  page: number;
-  results: TMDbSearchResult[];
-  total_pages: number;
-  total_results: number;
-}
